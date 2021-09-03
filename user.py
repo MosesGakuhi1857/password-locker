0f40ff18938dc756aa0a37cd850cc32e4198cdae
MosesGakuhi1857 <king'orimoses386@gmail.com>
2021-09-03 21:42:15 +0300

    Modified the file

---
+++ user.py
@@ -0,0 +1,11 @@
+class User:
+    """
+    class that generates new instances of user information
+    """
+    
+    def __init__(self,login_username,password):
+        
+        self.login_username=login_username
+        self.password=password
+    
+   
