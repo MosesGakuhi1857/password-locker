46f08f761252c8ad0b56105b2487351f5b754cf1
MosesGakuhi1857 <king'orimoses386@gmail.com>
2021-09-04 22:23:06 +0300

    Modified the file

--- credentials.py
+++ credentials.py
@@ -6,6 +6,21 @@ class Credentials:
     class that generates new instances of credentials instances
     """
     
+    credentials_list = []
+    user_credentials_list = []
+    
+    @classmethod
+    def check_user(cls,login_username,password):
+        """
+        method that checks if the name and password matches
+        """
+        current_user = ''
+        for user in User.user_list:
+            if(user.login_username == login_username and user.password == password):
+                current_user = user.login_username
+        return current_user
+    
+    
     def __init__(self,socialM,username,passcode):
         
         self.socialM = socialM
@@ -26,3 +41,11 @@ class Credentials:
         """
         gen_pass = ''.join(random.choice(char)for _ in range(size))
         return gen_pass
+    
+    @classmethod
+    def display_credentials(cls,socialM):
+        """
+        method that returns the account list
+        """
+        
+        user_credentials_list= []
